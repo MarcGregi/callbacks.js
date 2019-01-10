7331c3b73c321f5c2d7ec35b789cb325075f2b87
marcgregi <marc@hypedigital.co>
2019-01-10 20:14:01 +0000

    This task takes three comparative parameters to sort the below object values

--- sorting.js
+++ sorting.js
@@ -1,5 +1,18 @@
-arr = [10, 5, 2, 9, 1]
+// This task takes three comparative parameters to sort the below object values
 
-console.log(arr.sort(function(a,b){
-    return a - b
-}))
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+
+];
+
+results = students.sort(function(a,b) {
+  if(a.name > b.name) {
+    return a.name > b.name
+  } if(a.name === b.name) {
+    return b.age - a.age
+    }
+})
+console.log(results)
