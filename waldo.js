818808b3963075869afea9eb080453049b0fde7c
marcgregi <marc@hypedigital.co>
2019-01-10 17:36:32 +0000

    Refactor the function findWaldo to use the forEach() method instead of a for loop.

---
+++ waldo.js
@@ -0,0 +1,19 @@
+
+function findWaldo(arr, found) {
+  arr.forEach(function(item, index) {
+    if (item === "Waldo") {
+      found(index)
+    }
+  })
+}
+
+
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+
